8f3edb72caec7e298ce9edaf095b8b14496b4dd0
Nehal Bhautoo <66138528+Nehal-Bhautoo@users.noreply.github.com>
2021-01-18 21:06:33 +0400

    Initial Commit

---
+++ lib/carousel/CarouselSliderCard.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+class CarouselSliderCard extends StatefulWidget {
+  CarouselSliderCard() : super();
+
+  final String title = "Bluetooth Devices";
+
+  @override
+  State<StatefulWidget> createState() => CarouselSliderCardState();
+}
+
+class CarouselSliderCardState extends State<CarouselSliderCard> {
+  CarouselSlider carouselSlider;
+  int _current = 0;
+
+  List imgList = [
+    'https://images.unsplash.com/photo-1502117859338-fd9daa518a9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1554321586-92083ba0a115?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1536679545597-c2e5e1946495?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1543922596-b3bbaba80649?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1502943693086-33b5b1cfdf2f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80'
+  ];
+
+  List<T> map<T>(List list, Function handler) {
+    List<T> result = [];
+    for (var i = 0; i < list.length; i++) {
+      result.add(handler(i, list[i]));
+    }
+    return result;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white12,
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            carouselSlider = CarouselSlider(
+              height: 400.0,
+              initialPage: 0,
+              enlargeCenterPage: true,
+              autoPlay: true,
+              reverse: true,
+              enableInfiniteScroll: true,
+              autoPlayInterval: Duration(seconds: 2),
+              autoPlayAnimationDuration: Duration(milliseconds: 2000),
+              pauseAutoPlayOnTouch: Duration(seconds: 10),
+              scrollDirection: Axis.horizontal,
+              onPageChanged: (index) {
+                setState(() {
+                  _current = index;
+                });
+              },
+              items: imgList.map((imgUrl) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: EdgeInsets.symmetric(horizontal: 10.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white10,
+                      ),
+                      child: Image.network(
+                        imgUrl,
+                        fit: BoxFit.fill,
+                      ),
+                    );
+                  },
+                );
+              }).toList(),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: map<Widget>(imgList, (index, url) {
+                return Container(
+                  width: 10.0,
+                  height: 10.0,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: _current == index ? Colors.grey : Colors.blue,
+                  ),
+                );
+              }),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                OutlineButton(
+                  child: Text("Connect"),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,214 @@
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:surveillence_app/Animations/FadeAnimation.dart';
+import 'package:surveillence_app/carousel/CarouselSliderCard.dart';
+
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  )
+);
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+
+  AnimationController _scaleController;
+  AnimationController _scale2Controller;
+  AnimationController _widthController;
+  AnimationController _positionController;
+
+  Animation<double> _scaleAnimation;
+  Animation<double> _scale2Animation;
+  Animation<double> _widthAnimation;
+  Animation<double> _positionAnimation;
+
+  bool hideIcon = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _scaleController = AnimationController(
+        vsync: this,
+        duration: Duration(milliseconds: 300),
+    );
+    _scaleAnimation = Tween<double>(
+      begin: 1.0, end: 0.8).animate(_scaleController)..addStatusListener((status) {
+        if(status == AnimationStatus.completed) {
+          _widthController.forward();
+        }
+    });
+    _widthController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 600)
+    );
+
+    _widthAnimation = Tween<double>(
+      begin: 80.0,
+      end: 300.0
+    ).animate(_widthController)..addStatusListener((status) {
+      if(status == AnimationStatus.completed) {
+        _positionController.forward();
+      }
+    });
+
+    _positionController = AnimationController(
+        vsync: this,
+        duration: Duration(milliseconds: 1000)
+    );
+
+    _positionAnimation = Tween<double>(
+        begin: 0.0,
+        end: 215.0
+    ).animate(_positionController)..addStatusListener((status) {
+      if(status == AnimationStatus.completed) {
+        setState(() {
+          hideIcon = true;
+        });
+        _scale2Controller.forward();
+      }
+    });
+
+    _scale2Controller = AnimationController(
+        vsync: this,
+        duration: Duration(milliseconds: 1000)
+    );
+
+    _scale2Animation = Tween<double>(
+        begin: 1.0,
+        end: 32.0
+    ).animate(_scale2Controller)..addStatusListener((status) {
+      if(status == AnimationStatus.completed) {
+        Navigator.push(context, PageTransition(type: PageTransitionType.fade, child: CarouselSliderCard()));
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(3, 9, 23, 1),
+      body: Container(
+        width: double.infinity,
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              top: -50,
+              left: 0,
+              child: FadeAnimations(1, Container(
+                width: width,
+                height: 400,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/one.png'),
+                    fit: BoxFit.cover
+                  )
+                ),
+              )),
+            ),
+            Positioned(
+              top: -100,
+              left: 0,
+              child: FadeAnimations(1.3, Container(
+                width: width,
+                height: 400,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/one.png'),
+                      fit: BoxFit.cover
+                  )
+                ),
+              )),
+            ),
+            Positioned(
+              top: -150,
+              left: 0,
+              child: FadeAnimations(1.6, Container(
+                width: width,
+                height: 400,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/one.png'),
+                      fit: BoxFit.cover
+                  )
+                ),
+              )),
+            ),
+            Container(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  FadeAnimations(1, Text("Surveillance App",
+                  style: TextStyle(color: Colors.white, fontSize: 40),)),
+                  SizedBox(height: 15,),
+                  FadeAnimations(1.3, Text("Have an eye on your garden from a distance",
+                    style: TextStyle(color: Colors.white.withOpacity(.7), height: 1.4),
+                  )),
+                  SizedBox(height: 180,),
+                  FadeAnimations(1.6, AnimatedBuilder(
+                    animation: _scaleController,
+                    builder: (context, child) => Transform.scale(
+                    scale: _scaleAnimation.value,
+                    child: Center(
+                      child: AnimatedBuilder(
+                        animation: _widthController,
+                        builder: (context, child) => Container(
+                          width: _widthAnimation.value,
+                          height: 80,
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            color: Colors.blue.withOpacity(.4),
+                          ),
+                          child: InkWell(
+                            onTap: () {
+                              _scaleController.forward();
+                            },
+                            child: Stack(
+                              children: <Widget> [
+                              AnimatedBuilder(
+                                animation: _positionController,
+                                builder: (context, child) => Positioned(
+                                  left: _positionAnimation.value,
+                                  child: AnimatedBuilder(
+                                    animation: _scale2Controller,
+                                    builder: (context, child) => Transform.scale(
+                                      scale: _scale2Animation.value,
+                                      child: Container(
+                                        width: 60,
+                                        height: 60,
+                                        decoration: BoxDecoration(
+                                          shape: BoxShape.circle,
+                                          color: Colors.blue
+                                        ),
+                                        child: hideIcon == false ? Icon(Icons.arrow_forward, color: Colors.white,) : Container(),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              ]
+                            ),
+                          ),
+                        ),
+                      ),
+                    )),
+                  )),
+                  SizedBox(height: 60,),
+
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
