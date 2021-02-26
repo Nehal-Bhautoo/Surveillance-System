66791077a8930519dba44fb4f4bff1ed97dbf953
Nehal Bhautoo <66138528+Nehal-Bhautoo@users.noreply.github.com>
2021-02-26 14:04:07 +0400

    :zap: Add bluetooth dependency

--- lib/screens/connection/BluetoothScreen.dart
+++ lib/screens/connection/BluetoothScreen.dart
@@ -1,14 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
 
 class BluetoothScreenLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return _myListView(context);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: StreamBuilder<BluetoothState>(
+        stream: FlutterBlue.instance.state,
+        initialData: BluetoothState.unknown,
+        builder: (c, snapshot) {
+          final state = snapshot.data;
+          if(state == BluetoothState.on) {
+            return _myListView(context);
+          } return BluetoothOffScreen(state: state);
+        },
+      ),
+    );
+  }
+}
+
+class BluetoothOffScreen extends StatelessWidget {
+  const BluetoothOffScreen({Key key, this.state}) : super(key: key);
+  final BluetoothState state;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Icon(
+              Icons.bluetooth_disabled,
+              size: 200.0,
+              color: Colors.white54,
+            ),
+            Text(
+              'Bluetooth Adapter is ${state != null ? state.toString().substring(15) : 'not available'}.',
+              style: Theme.of(context)
+              .primaryTextTheme
+              .subtitle1
+              .copyWith(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
 Widget _myListView(BuildContext context) {
-  final titles = ['bike', 'boat', 'bus', 'car', 'railway', 'run', 'subway', 'transit', 'walk'];
+  final titles = ['Nehal', 'Bhautoo', 'Middlesex'];
   final colors = [
     Colors.amber,
     Colors.cyan,
