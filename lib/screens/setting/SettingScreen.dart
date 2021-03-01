2e912414b3ef24d7e0090ae9a12f3e9f602a7446
Nehal Bhautoo <66138528+Nehal-Bhautoo@users.noreply.github.com>
2021-03-01 14:00:28 +0400

    :zap: Add Appbar

--- lib/screens/setting/SettingScreen.dart
+++ lib/screens/setting/SettingScreen.dart
@@ -7,21 +7,30 @@ class SettingScreen extends StatelessWidget {
       title: "Setting",
       darkTheme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SettingScreenPage(),
     );
   }
 }
 
 class SettingScreenPage extends StatefulWidget {
+  SettingScreenPage({Key key}) : super(key: key);
   @override
   _SettingScreenPageState createState() => _SettingScreenPageState();
-
 }
 
 class _SettingScreenPageState extends State<SettingScreenPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Settings"),
+      ),
+      body: Center(
 
+      ),
     );
   }
 }
